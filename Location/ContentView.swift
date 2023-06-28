22ff324a1e69ce8cb0a1aa8e6e636e506d4bfdb9
Jan Hovland <jan.hovland@lyse.net>
2023-06-28 15:22:39 +0200

    Initial Commit

---
+++ Location/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Location
+//
+//  Created by Jan Hovland on 28/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Location/LocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationApp.swift
+//  Location
+//
+//  Created by Jan Hovland on 28/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct LocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
