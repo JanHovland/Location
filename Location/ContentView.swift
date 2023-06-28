f14a546c2e238aa5225c4b7fc976468cff3f4fe6
Jan Hovland <jan.hovland@lyse.net>
2023-06-28 16:02:37 +0200

    Update

--- Location/ContentView.swift
+++ Location/ContentView.swift
@@ -7,18 +7,75 @@
 
 import SwiftUI
 
+import os
+import CoreLocation
+
 struct ContentView: View {
+    @ObservedObject var locationsHandler = LocationsHandler.shared
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Text("Location: \(locationsHandler.lastLocation.coordinate.latitude) \(locationsHandler.lastLocation.coordinate.longitude)")
+                .padding(10)
+            Spacer()
         }
-        .padding()
+        .onAppear {
+            self.locationsHandler.startLocationUpdates()
+        }
     }
 }
 
+
 #Preview {
     ContentView()
+}
+
+@MainActor class LocationsHandler: ObservableObject {
+    let logger = Logger(subsystem: "com.janHovland.liveUpdatesSample", category: "LocationsHandler")
+    
+    static let shared = LocationsHandler()  // Create a single, shared instance of the object.
+
+    private let manager: CLLocationManager
+    private var background: CLBackgroundActivitySession?
+
+    @Published var lastLocation = CLLocation()
+
+    @Published
+    var updatesStarted: Bool = UserDefaults.standard.bool(forKey: "liveUpdatesStarted") {
+        didSet { UserDefaults.standard.set(updatesStarted, forKey: "liveUpdatesStarted") }
+    }
+    
+    private init() {
+        self.manager = CLLocationManager()  // Creating a location manager instance is safe to call here in `MainActor`.
+
+    }
+    
+    func startLocationUpdates() {
+        if self.manager.authorizationStatus == .notDetermined {
+            self.manager.requestWhenInUseAuthorization()
+        }
+        Task() {
+            do {
+                self.updatesStarted = true
+                let updates = CLLocationUpdate.liveUpdates()
+                for try await update in updates {
+                    if !self.updatesStarted { break }  // End location updates by breaking out of the loop.
+                    if let loc = update.location {
+                        self.lastLocation = loc
+                        if self.lastLocation.coordinate.latitude != 0.00 {
+                            stopLocationUpdates()
+                        }
+                    }
+                }
+            } catch {
+                self.logger.error("Could not start location updates")
+            }
+            return
+        }
+    }
+    
+    func stopLocationUpdates() {
+        self.updatesStarted = false
+    }
 }
