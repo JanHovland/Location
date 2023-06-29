b4e901ece8ab65a194db6bcfcf6b03b79a89c7c3
Jan Hovland <jan.hovland@lyse.net>
2023-06-29 08:43:00 +0200

    Update

--- Location/ContentView.swift
+++ Location/ContentView.swift
@@ -6,59 +6,12 @@
 //
 
 import SwiftUI
-import os
-import CoreLocation
 
 ///
 /// Globale variabler
 ///
 var latitude: Double = 0.00
 var longitude: Double = 0.00
-
-@MainActor class LocationsHandler: ObservableObject {
-    
-    static let shared = LocationsHandler()
-    private let manager: CLLocationManager
-    @Published
-    var lastLocation = CLLocation()
-    @Published
-    var updatesStarted: Bool = UserDefaults.standard.bool(forKey: "liveUpdatesStarted") {
-        didSet { UserDefaults.standard.set(updatesStarted, forKey: "liveUpdatesStarted") }
-    }
-    
-    private init() {
-        self.manager = CLLocationManager()
-    }
-    
-    func startLocationUpdates() {
-        if self.manager.authorizationStatus == .notDetermined {
-            self.manager.requestWhenInUseAuthorization()
-        }
-        Task() {
-            do {
-                self.updatesStarted = true
-                let updates = CLLocationUpdate.liveUpdates()
-                for try await update in updates {
-                    if !self.updatesStarted { break }
-                    if let loc = update.location {
-                        self.lastLocation = loc
-                        if self.lastLocation.coordinate.latitude != 0.00 {
-                            ///
-                            /// Oppdaterer de globale variablene
-                            ///
-                            latitude = self.lastLocation.coordinate.latitude
-                            longitude = self.lastLocation.coordinate.longitude
-                            self.updatesStarted = false
-                        }
-                    }
-                }
-            } catch {
-                debugPrint(error)
-            }
-            return
-        }
-    }
-}
 
 struct ContentView: View {
     @ObservedObject var locationsHandler = LocationsHandler.shared
